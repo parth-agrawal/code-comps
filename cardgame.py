c1cf540d71702d79cd8f217493600503513fd876
Parth Agrawal <parth.agrawal@outlook.com>
2020-01-02 10:08:08 -0500

    First commit

---
+++ cardgame.py
@@ -0,0 +1,16 @@
+# https://codeforces.com/contest/1270/problem/A
+
+def main():
+    
+    from sys import stdin
+    
+    input = iter(stdin.read().split())
+    
+    t = int(input.next())
+
+    # iterates over test cases
+    for i in range(0, t):
+        n, k1, k2 = (int(input.next()), int(input.next()), int(input.next))
+        p1_cards = input.next.split()
+        p2_cards = input.next.split()
+    
